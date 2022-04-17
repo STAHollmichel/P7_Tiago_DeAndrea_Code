0a1a4daa09cf4d58e56392fd069e428546403759
Tiago de Andrea <sta.holllmichel@gmail.com>
2022-04-17 19:04:08 +0100

    update

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,69 +1,96 @@
 const db = require('../models/index');
 
 const fs = require('fs');
+const { post } = require('../routes/comments');
 
 const { Post } = db.sequelize.models;
 
 
-// exports.createPost = async (req, res, next) => {
-//     console.log(req.body);
-//     const newPost = new Post({
-//         ...req.body,
-//         // imageUrl: `${ req.protocol }://${ req.get("host") }/images/${ req.file.filename }`,
-//     });
-//         newPost.save()
-//             .then(() => res.status(201).json({ message: 'Post enregistrée !'}))
+exports.createPost = async (req, res, next) => {
+    console.log(req.body);
+    const newPost = new Post({
+        ...req.body,
+        // imageUrl: `${ req.protocol }://${ req.get("host") }/images/${ req.file.filename }`,
+    });
+        newPost.save()
+            .then(() => res.status(201).json({ message: 'Post enregistrée !'}))
+            .catch(error => res.status(400).json({ error }));
+};
+
+// exports.createPost = (req, res, next) => {
+//     Post.create ({  
+//         postTittle: req.body.postTittle,  
+//         postDescription: req.body.postDescription,
+//         postPhoto: req.file ? `${ req.protocol }://${ req.get("host") }/images/${ req.file.filename }` : null,
+//         userId: req.body.userId,
+//     })  
+//         .then(() => res.status(201).json({ message: 'Post enregistrée !'}))
+//         .catch(error => res.status(400).json({ error }));
+// };
+
+
+
+// exports.modifyPost = (req, res, next) => {
+    // const postObject = req.file ? 
+    //     { 
+    //         ...JSON.parse(req.body.post),
+    //         // imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+    //     } 
+    //     : { ...req.body };
+    // const post=
+//     Post.update({ where: {id: req.params.id}}, { ...req.body})
+//         .then(() => res.status(200).json({ message: 'Post modifiée !'}))
+//         .catch(error => res.status(400).json({ error }));
+// };
+
+
+
+// exports.modifyPost = (req, res, next) => {
+//  const updatePost (req.body) = {(
+//         postTittle: post.postTittle,
+//         postDescription: post.postDescription,
+//         postPhoto: post.postPhoto,
+//     }
+//     return Post.update(updatePost, { where: {id: req.params.id}})
+//             .then(() => res.status(200).json({ message: 'Post modifiée !'}))
 //             .catch(error => res.status(400).json({ error }));
 // };
 
-exports.createPost = (req, res, next) => {
-    Post.create ({  
-        postTittle: req.body.postTittle,  
-        postDescription: req.body.postDescription,
-        postPhoto: req.file ? `${ req.protocol }://${ req.get("host") }/images/${ req.file.filename }` : null,
-        userId: req.body.userId,
-    })  
-        .then(() => res.status(201).json({ message: 'Post enregistrée !'}))
-        .catch(error => res.status(400).json({ error }));
-};
-
-
-
 exports.modifyPost = (req, res, next) => {
-    const postObject = req.file ? 
+    const updatePost= req.file ? 
         { 
-            ...JSON.parse(req.body.post),
-            imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+            ...JSON.parse(req.body),
+            // imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
         } 
         : { ...req.body };
-    Post.updateOne({ _id: req.params.id }, { ...postObject, _id: req.params.id })
+    Post.update({  where: { id: req.params.id }}, { ...updatePost,  where: { id: req.params.id }})
         .then(() => res.status(200).json({ message: 'Post modifiée !'}))
         .catch(error => res.status(400).json({ error }));
-};
+  };
 
 exports.deletePost = (req, res, next) => {
-    Post.findOne({ _id: req.params.id })
+    console.log(req.params.id);
+    Post.findOne({ where: {id: req.params.id}})
         .then(post => {
-            const filename = post.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-                Post.deleteOne({ _id: req.params.id })
+            console.log(post);
+            // const filename = post.imageUrl.split('/images/')[1];
+            // fs.unlink(`images/${filename}`, () => {
+                Post.destroy({ where: {id: req.params.id}})
                     .then(() => res.status(200).json({ message: 'Post supprimée !'}))
                     .catch(error => res.status(400).json({ error }));
-            });
+            // });
         })
         .catch(error => res.status(500).json({ error }));
   };
   
 exports.getAllPosts = (req, res, next) => {
-    Post.find()
+    Post.findAll()
         .then((posts) => res.status(200).json(posts))
         .catch(error => res.status(400).json({ error }));
 };
-  
 
 exports.getOnePost = (req, res, next) => {
-    Post.findOne({ _id: req.params.id })
+    Post.findOne({ where: {id: req.params.id }})
         .then((post) => res.status(200).json(post))
         .catch(error => res.status(400).json({ error }));
 };
-  
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,6 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    console.log("Auth middleware" , userId);
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
